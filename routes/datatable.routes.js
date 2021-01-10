90c9a45f81b93eb9d55d3ca828993d983db45b26
Lucija Perkovic <lp51062@fer.hr>
2021-01-10 19:57:09 +0100

    added_CRUD

--- routes/datatable.routes.js
+++ routes/datatable.routes.js
@@ -20,12 +20,56 @@ router.get('/JSONDownload', async function(req,res){
     res.json(nac_parkovi);
     
 });
-router.get('/CSVDownload', async function(req,res){
-    const nac_parkovi = (await db.query('SELECT * FROM nacionalni_parkovi'));
-    
-    res.send(nac_parkovi);
-    
-    
-    
-});
+
+router.delete('/delete/:id', async function(req,res){
+    console.log('YO')
+    var id = req.body.id;
+    db.query(`DELETE FROM nacionalni_parkovi WHERE id = $1`, [id]);
+
+})
+
+
+router.put('/edit/:id', async function(req,res){
+    var id = req.body.id;
+    var naziv_parka = req.body.naziv_parka;
+    var drzava = req.body.drzava;
+    var lokacija = req.body.lokacija;
+    var godina_osnutka = req.body.godina_osnutka;
+    var wikipedia_stranica = req.body.wikipedia_stranica;
+    var prosj_broj = req.body.prosj_broj;
+    var povrsina = req.body.povrsina;
+    var nativna_vrsta = req.body.nativna_vrsta;
+    var klima = req.body.klima;
+    var sluzbena_stranica = req.body.sluzbena_stranica;
+    db.query(`UPDATE nacionalni_parkovi 
+              SET nazivparka = $1,
+                  drzava = $2,
+                  lokacija = $3,
+                  wikipediastranica = $4,
+                  prosjbrposjetiteljagodisnje = $5,
+                  povrsina = $6,
+                  sluzbenastranica = $7,
+                  klima = $8,
+                  godinaosnutka = $9,
+                  nativnavrsta = $10   
+              WHERE id = $11`, [naziv_parka,drzava, lokacija, wikipedia_stranica, prosj_broj, povrsina, sluzbena_stranica,
+                klima, godina_osnutka, nativna_vrsta,id]);
+
+})
+router.post('/add', async function(req,res){
+    var naziv_parka = req.body.naziv_parka;
+    var drzava = req.body.drzava;
+    var lokacija = req.body.lokacija;
+    var godina_osnutka = req.body.godina_osnutka;
+    var wikipedia_stranica = req.body.wikipedia_stranica;
+    var prosj_broj = req.body.prosj_broj;
+    var povrsina = req.body.povrsina;
+    var nativna_vrsta = req.body.nativna_vrsta;
+    var klima = req.body.klima;
+    var sluzbena_stranica = req.body.sluzbena_stranica;
+    db.query(`INSERT INTO nacionalni_parkovi (nazivparka, drzava,lokacija, wikipediastranica, prosjbrposjetiteljagodisnje, povrsina, sluzbenastranica, klima,godinaosnutka,nativnavrsta)
+            VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`, [naziv_parka,drzava, lokacija, wikipedia_stranica, prosj_broj, povrsina, sluzbena_stranica,
+                klima, godina_osnutka, nativna_vrsta]);
+
+})
 module.exports = router;
--- server.js
+++ server.js
@@ -3,10 +3,7 @@ const app = express();
 const path = require('path');
 
 const datatableRouter = require('./routes/datatable.routes');
-const add_parkRouter = require('./routes/add_park.routes');
-const update_parkRouter = require('./routes/update_park.routes');
-const delete_parkRouter = require('./routes/delete_park.routes');
-const get_parkRouter = require('./routes/get_park.routes');
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -15,9 +12,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}));
 
 app.use('/', datatableRouter);
-app.use('/add_park', add_parkRouter);
-app.use('/update_park', update_parkRouter);
-app.use('/delete_park', delete_parkRouter);
-app.use('/get_park', get_parkRouter);
 
-app.listen(4000);
+
+app.listen(3000);
